57d7bb8658d1d46c0670540cbf43fd81ac366b3f
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-10 15:49:52 -0500

    Updated Contact page content

---
+++ my-next-app/pages/contact.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Contact Us</h1>
+            <p>Email: example@example.com</p>
+        </div>
+    );
+}
