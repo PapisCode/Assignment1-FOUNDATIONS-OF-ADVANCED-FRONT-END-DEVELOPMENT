da7b87ba83c6155d7bf28c444f51a7749391ca7b
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-10 15:46:29 -0500

    Updated About pages content

---
+++ my-next-app/pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function About() {
+    return (
+        <div>
+        <Navbar />
+        <h1>About Us</h1>
+        <p>This is a simple Next.js application which demonstrates React concepts</p>
+        </div>
+    );
+}
